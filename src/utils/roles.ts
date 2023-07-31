9eafe5ac2e570da427be4e825bcfae83612f07e0
Caio Stoduto <caio.stoduto@outlook.com>
2023-07-30 23:13:03 -0300

    refactor: moving db guild settings
    moving db guild settings from "settings/{guild_id}/automatic_role" to
    "guilds/{guild_id}/settings/auto_role"

--- src/utils/roles.ts
+++ src/utils/roles.ts
@@ -5,15 +5,17 @@ export default function addRolesIfNeeded(bot: Bot, members: GuildMember[]) {
   members = members.filter((member) => !member.user.bot);
   if (members.length == 0) return;
 
-  bot.database.doc(`settings/${members[0]!.guild.id}`).get().then((doc) => {
-    if (!doc || !doc.data() || !doc.data()!.automatic_roles) return;
+  bot.database.doc(`guilds/${members[0]!.guild.id}`).get().then((doc) => {
+    if (!doc.data() || !doc.data()!.settings || !doc.data()!.settings.auto_role) {
+      return;
+    }
 
-    var automatic_roles = doc.data()!.automatic_roles
+    var auto_role = doc.data()!.settings.auto_role
       .map((role: string) => members[0]!.guild.roles.cache.get(role))
       .filter((role: Role) => role != undefined) as Role[]
 
     members.forEach((member) => {
-      var new_roles = automatic_roles
+      var new_roles = auto_role
         .filter((role: Role) => !member.roles.cache.has(role.id));
 
       if (new_roles.length > 0) {
