3c6ac0163aa0bd26fe16befc15a39b2e2c925a32
Caio Stoduto <caio.stoduto@outlook.com>
2023-08-30 02:45:49 -0300

    fix(invites): guildMemberAdd/invites not handling event & /convite exceding max setNickname length

--- src/events/guildMemberAdd/invites.ts
+++ src/events/guildMemberAdd/invites.ts
@@ -1,8 +1,8 @@
-import { GuildMember } from "discord.js"
-import Bot from "../../bot"
-import { Event } from "../eventHandler"
-import VolunteerModel from "../../models/volunteer"
-import GuildModel from "../../models/guild"
+import { type GuildMember } from 'discord.js'
+import type Bot from '../../bot'
+import { type Event } from '../eventHandler'
+import VolunteerModel from '../../models/volunteer'
+import GuildModel from '../../models/guild'
 
 export default class Invites implements Event {
   data = {
@@ -10,34 +10,49 @@ export default class Invites implements Event {
     once: false
   }
 
-  async execute(bot: Bot, member: GuildMember) {
+  async execute (bot: Bot, member: GuildMember): Promise<undefined> {
     member.guild.invites.fetch().then((newInvites) => {
-      var oldInvites = bot.invites.get(member.guild.id)!
+      const oldInvites = bot.invites.get(member.guild.id)
+      if (oldInvites === undefined) return
 
-      var invite = newInvites.find(i => i.uses! > oldInvites.get(i.code)!);
-      if (!invite) return;
+      const invite = newInvites
+        .find(i =>
+          i.uses !== null &&
+          oldInvites.get(i.code) !== null &&
+          i.uses > (oldInvites.get(i.code) as number))
+
+      if (invite == null) return
 
       GuildModel.findOne({ guild_id: member.guild.id }).then(guild => {
-        if (!guild || !guild.invites) return
+        if (guild?.invites == null) return
 
-        var invite_db = guild.invites.find((i) => i.code! == invite!.code!)
-        if (!invite_db) return
+        const inviteDB = guild.invites.find((i) => i.code === invite.code)
+        if (inviteDB == null) return
 
-        VolunteerModel.findOne({ ra: invite_db?.ra }).then(volunteer => {
-          if (!volunteer) return
-          var names = volunteer.nome!.split(" ")
-
-          var nickname = `${names.shift()} ` +
+        VolunteerModel.findOne({ ra: inviteDB.ra }).then(volunteer => {
+          if (volunteer?.nome == null) return
+          const names = volunteer.nome.split(' ')
+          const nickname = `${names.shift()} ` +
             `${names
-              .map((name) => name.split("")[0])
-              .filter((name) => name == name.toUpperCase())
-              .join(". ")}.`
+              .map((name) => name.split('')[0])
+              .filter((name) => name === name.toUpperCase())
+              .join('. ')}.`
 
           member.setNickname(nickname)
-          volunteer.discord.push(member.id)
-          volunteer.save()
-        })
-      })
-    })
+            .catch((err) => { bot.logger.error((err as Error).stack) })
+
+          if (guild.volunteer_role_id != null) {
+            member.roles.add(guild.volunteer_role_id)
+              .catch((err) => { bot.logger.error((err as Error).stack) })
+          }
+
+          if (!volunteer.discord.includes(member.id)) {
+            volunteer.discord.push(member.id)
+          }
+
+          volunteer.save().catch((err) => { bot.logger.error((err as Error).stack) })
+        }).catch((err) => { bot.logger.error((err as Error).stack) })
+      }).catch((err) => { bot.logger.error((err as Error).stack) })
+    }).catch((err) => { bot.logger.error((err as Error).stack) })
   }
 }
